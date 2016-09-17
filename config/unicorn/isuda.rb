342a2c6ad4e70ec497d5ca12af159695ced0a717
takanamito <takanamito0928@gmail.com>
2016-09-17 12:22:21 +0900

    unicorn config

--- config/unicorn/isuda.rb
+++ config/unicorn/isuda.rb
@@ -1,6 +1,10 @@
 worker_processes 5
-preload_app true
 timeout 120
+
+shared_path = File.expand_path('shared')
+listen File.expand_path('tmp/isuda.sock', shared_path)
 
 stdout_path File.expand_path('log/isuda.log')
 stderr_path File.expand_path('log/isuda.log')
+
+preload_app true
--- config/unicorn/isutar.rb
+++ config/unicorn/isutar.rb
@@ -1,6 +1,10 @@
 worker_processes 5
-preload_app true
 timeout 120
+
+shared_path = File.expand_path('shared')
+listen File.expand_path('tmp/isutar.sock', shared_path)
 
 stdout_path File.expand_path('log/isutar.log')
 stderr_path File.expand_path('log/isutar.log')
+
+preload_app true
