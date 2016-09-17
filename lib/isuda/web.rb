3390aa12a27e5630aeff64d36502832b7e19dd9e
takanamito <takanamito0928@gmail.com>
2016-09-17 17:55:23 +0900

    hoge

--- lib/isuda/web.rb
+++ lib/isuda/web.rb
@@ -93,6 +93,7 @@ module Isuda
         validation['valid']
         ! validation['valid']
       end
+      memoize :is_spam_content
 
       def cached_patterns
         redis_get('patterns') || refresh_patterns
