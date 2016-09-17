9581fbdd6ef041637a95393e959d70cc2947f68d
aileron.cc <aileron.cc@gmail.com>
2016-09-17 17:38:52 +0900

    Delete

--- config/unicorn/isutar.rb
+++
@@ -1,9 +0,0 @@
-worker_processes 5
-timeout 120
-
-listen File.expand_path('tmp/isutar.sock')
-
-stdout_path File.expand_path('log/isutar.log')
-stderr_path File.expand_path('log/isutar.log')
-
-preload_app true
